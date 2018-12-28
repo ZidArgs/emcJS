b6c59e828c86340ccb5df5e562e670c8da305c94
Denis Peters <denis.peters@udo.edu>
2018-12-28 01:29:21 +0100

    added ini parser to file loader

--- util/FileLoader.mjs
+++ util/FileLoader.mjs
@@ -1,3 +1,7 @@
+const LNBR_SEQ = /(?:\r\n|\n|\r)/g;
+const INI_GRP = /^\[(.+)\]$/;
+const INI_VAL = /^[^=]+=[^=]*$/;
+
 class FileLoader {
 
     json(file) {
@@ -10,9 +14,7 @@ class FileLoader {
             }),
             mode: 'cors',
             cache: 'default'
-        })).then(function(r) {
-            return r.json();
-        }.bind(this));
+        })).then(r => r.json());
     }
 
     text(file) {
@@ -25,9 +27,43 @@ class FileLoader {
             }),
             mode: 'cors',
             cache: 'default'
-        })).then(function(r) {
-            return r.text();
-        }.bind(this));
+        })).then(r => r.text());
+    }
+
+    ini(file) {
+        return fetch(new Request(file, {
+            method: 'GET',
+            headers: new Headers({
+                "Content-Type": "text/plain",
+                "Pragma": "no-cache",
+                "Cache-Control": "no-cache"
+            }),
+            mode: 'cors',
+            cache: 'default'
+        })).then(r => r.text()).then(r => {
+            var
+                res = {},
+                act = "",
+                lines = ini.split(LNBR_SEQ);
+            for(let i = 0; i < lines.length; ++i) {
+                var line = lines[i];
+                if(line.startsWith(";") || !line.length) {
+                    continue;
+                }
+                if(INI_GRP.test(line)) {
+                    act = line.slice(1, -1);
+                    res[act] = res[act] || {};
+                    continue;
+                }
+                if(INI_VAL.test(line)) {
+                    var data = line.split("=");
+                    res[act][data[0]] = data[1];
+                    continue;
+                }
+                throw new Error("parse error at line " + i + ": " + line);
+            }
+            return res;
+        });
     }
 
 }
