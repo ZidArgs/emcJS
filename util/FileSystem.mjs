e605a4585ebdd632c612f9066d016868eaa373b5
Denis Peters <denis.peters@udo.edu>
2019-01-08 07:00:04 +0100

    fix element add to dom before click, not after

--- util/FileSystem.mjs
+++ util/FileSystem.mjs
@@ -1,41 +1,40 @@
 const dl = document.createElement("a");
-dl.style = "display: none";
+dl.style.position = "absolute !important";
+dl.style.display = "none !important";
+dl.style.opacity = "0 !important";
+dl.style.visibility = "hidden !important";
 
 const ul = document.createElement("input");
 ul.setAttribute("type", "file");
-ul.style = "display: none";
-
-function loadPromise(resolve, reject) {
-    ul.onchange = loadChange.bind(this, resolve, reject);
-    ul.onerror = reject;
-    ul.click();
-}
-
-function loadChange(resolve, reject) {
-    var file = this.files[0];
-    var reader = new FileReader();
-    reader.onload = loadOnload.bind(this, resolve);
-    reader.onabort = resolve;
-    reader.onerror = reject;
-    reader.readAsDataURL(file);
-};
-
-function loadOnload(resolve, e) {
-    resolve(e.target.result);
-}
+ul.style.position = "absolute !important";
+ul.style.display = "none !important";
+ul.style.opacity = "0 !important";
+ul.style.visibility = "hidden !important";
 
 class FileSystem {
 
     load() {
-        return new Promise(loadPromise);
+        return new Promise((resolve, reject) => {
+            ul.onchange = e => {
+                let reader = new FileReader();
+                reader.onload = e => resolve(e.target.result);
+                reader.onabort = resolve;
+                reader.onerror = reject;
+                reader.readAsDataURL(e.files[0]);
+            };
+            ul.onerror = reject;
+            ul.click();
+        });
     }
 
     save(data, fileName) {
-        var url = window.URL.createObjectURL(new Blob([data], {type: "octet/stream"}));
+        let url = window.URL.createObjectURL(new Blob([data], {type: "octet/stream"}));
         dl.href = url;
         dl.download = fileName;
+        document.body.appendChild(dl);
         dl.click();
         window.URL.revokeObjectURL(url);
+        document.body.removeChild(dl);
     }
 
 }
