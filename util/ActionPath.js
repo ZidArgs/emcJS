731f5de75593bb9c7207322ca9550d1c6e79a99c
Denis Peters <denis.peters@udo.edu>
2020-01-24 10:29:38 +0100

    added actionpath utility

---
+++ util/ActionPath.js
@@ -0,0 +1,46 @@
+const ACTION_LIST = new WeakMap();
+const POINTER = new WeakMap();
+
+export default class ActionPath {
+
+    constructor() {
+        this.clear();
+    }
+
+    put(step) {
+        let pointer = POINTER.get(this);
+        let actions = ACTION_LIST.get(this);
+        actions[pointer++] = step;
+        if (pointer < actions.length) {
+            actions = actions.slice(0, pointer);
+            ACTION_LIST.set(this, actions);
+        }
+        POINTER.set(this, pointer);
+    }
+
+    clear() {
+        ACTION_LIST.set(this, []);
+        POINTER.set(this, 0);
+    }
+
+    redo() {
+        let pointer = POINTER.get(this);
+        let actions = ACTION_LIST.get(this);
+        if (actions.length > 0 && pointer < actions.length) {
+            let res = actions[pointer++];
+            POINTER.set(this, pointer);
+            return res;
+        }
+    }
+
+    undo() {
+        let pointer = POINTER.get(this);
+        let actions = ACTION_LIST.get(this);
+        if (actions.length > 0 && pointer > 0) {
+            let res = actions[--pointer];
+            POINTER.set(this, pointer);
+            return res;
+        }
+    }
+
+}
