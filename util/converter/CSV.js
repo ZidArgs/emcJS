9773794d16e6de8c5b4b9f8d6910a1fe20c12213
Denis Peters <denis.peters@udo.edu>
2020-06-18 22:59:05 +0200

    added CSV converter;

---
+++ util/converter/CSV.js
@@ -0,0 +1,34 @@
+const LNBR_SEQ = /(?:\r\n|\n|\r)/g;
+
+class CSV {
+
+    parse(input, split = ",") {
+        return input.split(LNBR_SEQ).map(item => {
+            let output = [];
+            let entries = item.split(split);
+            parseEntries:
+            while (entries.length) {
+                let act = entries.shift();
+                if (!act.startsWith('"')) {
+                    output.push(act);
+                } else {
+                    let buf = [act];
+                    while (entries.length) {
+                        act = entries.shift();
+                        buf.push(act);
+                        if (act.endsWith('"')) {
+                            output.push(buf.join(",").replace(/^"|"$/g, '').replace(/""/g, '"'));
+                            buf = [];
+                            continue parseEntries;
+                        }
+                    }
+                    output.push(buf.join(","));
+                }
+            }
+            return output;
+        })
+    }
+
+}
+
+export default new CSV();
